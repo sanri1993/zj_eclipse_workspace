49da00854db818497fdfb7e7855d7ee2aae173ee
zhengjin <zivieira@163.com>
2019-11-05 17:45:54 +0800

    Add cobertura code coverage in ci workflow.

--- zj_maven_project/run.sh
+++ zj_maven_project/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -uex
 
-if [ $1 == "main" ]; then
+if [[ $1 == "main" ]]; then
   # build hadoop jar package
   # before check maven.compiler.source=1.7
   echo "run maven clean and package."
@@ -9,10 +9,24 @@ if [ $1 == "main" ]; then
   mv target/zj-mvn-demo.jar /tmp/hadoop_test
 fi
 
-if [ $1 == "check" ]; then
+if [[ $1 == "check" ]]; then
   echo "run checkstyle."
-  #mvn checkstyle:checkstyle
-  mvn site:site
+  mvn checkstyle:checkstyle
+fi
+
+if [[ $1 == "cover" ]]; then
+  echo "run code coverage."
+  mvn clean cobertura:cobertura
+fi
+
+if [[ $1 == "checkall" ]]; then
+  echo "run checkstyle and coverage."
+  mvn clean checkstyle:checkstyle cobertura:cobertura
+fi
+
+if [[ $1 == "site" ]]; then
+  echo "run project info site."
+  mvn clean site:site
 fi
 
 echo "Java build DONE."
