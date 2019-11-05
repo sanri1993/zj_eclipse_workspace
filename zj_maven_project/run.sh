17208974e2fa8db1831156b751d22b8db3bf6411
zhengjin <zivieira@163.com>
2019-11-05 09:47:20 +0800

    Add checkstyle mvn workflow.

--- zj_maven_project/run.sh
+++ zj_maven_project/run.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 set -uex
 
-# build hadoop jar package
-# before check maven.compiler.source=1.7
-mvn clean package
-mv target/zj-mvn-demo.jar /tmp/hadoop_test
+if [ $1 == "main" ]; then
+  # build hadoop jar package
+  # before check maven.compiler.source=1.7
+  echo "run maven clean and package."
+  mvn clean package
+  mv target/zj-mvn-demo.jar /tmp/hadoop_test
+fi
 
+if [ $1 == "check" ]; then
+  echo "run checkstyle."
+  #mvn checkstyle:checkstyle
+  mvn site:site
+fi
+
+echo "Java build DONE."
 
 set +uex
