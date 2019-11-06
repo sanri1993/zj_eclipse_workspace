a5c629aa492cfac3ba6b8c6b8cdddb40c52e2230
zhengjin <zivieira@163.com>
2019-11-06 22:51:29 +0800

    Fix and update maven settings for ci workflow.

--- zj_maven_project/run.sh
+++ zj_maven_project/run.sh
@@ -4,7 +4,7 @@ set -uex
 if [[ $1 == "main" ]]; then
   # build hadoop jar package
   # before check maven.compiler.source=1.7
-  echo "run maven clean and package."
+  echo "run clean and package."
   mvn clean package
   mv target/zj-mvn-demo.jar /tmp/hadoop_test
 fi
@@ -19,15 +19,16 @@ if [[ $1 == "cober" ]]; then
   mvn clean cobertura:cobertura
 fi
 
-if [[ $1 == "jacoco"  ]]; then
+if [[ $1 == "jacoco" ]]; then
   echo "run jacoco code coverage."
   # mvn help:describe -Dplugin=org.jacoco:jacoco-maven-plugin -Ddetail
-  mvn clean test org.jacoco:jacoco-maven-plugin:prepare-agent install -Dmaven.test.failure.ignore=true
+  mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install -Dmaven.test.failure.ignore=true
 fi
 
 if [[ $1 == "checkall" ]]; then
-  echo "run checkstyle and coverage."
-  mvn clean checkstyle:checkstyle org.jacoco:jacoco-maven-plugin:prepare-agent install -Dmaven.test.failure.ignore=true
+  echo "run checkstyle and code coverage."
+  mvn clean checkstyle:checkstyle \
+  org.jacoco:jacoco-maven-plugin:prepare-agent install -Dmaven.test.failure.ignore=true
 fi
 
 if [[ $1 == "site" ]]; then
@@ -35,6 +36,13 @@ if [[ $1 == "site" ]]; then
   mvn clean site:site
 fi
 
-echo "Java build DONE."
+if [[ $1 == "sonar" ]]; then
+  echo "run junit, checkstyle and code coverage, and push project data to sonarqube."
+  mvn clean checkstyle:checkstyle \
+  org.jacoco:jacoco-maven-plugin:prepare-agent install -Dmaven.test.failure.ignore=true \
+  sonar:sonar -Dsonar.host.url=http://localhost:9000
+fi
+
+echo "Java project ci DONE."
 
 set +uex
