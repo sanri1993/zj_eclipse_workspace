6c754970e068da5707f4d0c1db4847406b6174c1
zhengjin <zivieira@163.com>
2019-11-07 15:04:41 +0800

    fix ci configs.

--- zj_maven_project/run.sh
+++ zj_maven_project/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -uex
 
-if [[ $1 == "main" ]]; then
+if [[ $1 == "jar" ]]; then
   # build hadoop jar package
   # before check maven.compiler.source=1.7
   echo "run clean and package."
@@ -16,30 +16,30 @@ fi
 
 if [[ $1 == "cober" ]]; then
   echo "run cobertura code coverage."
-  mvn clean cobertura:cobertura
+  mvn clean cobertura:check # invoke custom goal "check"
 fi
 
 if [[ $1 == "jacoco" ]]; then
   echo "run jacoco code coverage."
   # mvn help:describe -Dplugin=org.jacoco:jacoco-maven-plugin -Ddetail
-  mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install -Dmaven.test.failure.ignore=true
+  mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent test -Dmaven.test.failure.ignore=true
 fi
 
 if [[ $1 == "checkall" ]]; then
   echo "run checkstyle and code coverage."
   mvn clean checkstyle:checkstyle \
-  org.jacoco:jacoco-maven-plugin:prepare-agent install -Dmaven.test.failure.ignore=true
+  org.jacoco:jacoco-maven-plugin:prepare-agent test -Dmaven.test.failure.ignore=true
 fi
 
 if [[ $1 == "site" ]]; then
-  echo "run project info site."
+  echo "run project info report site."
   mvn clean site:site
 fi
 
 if [[ $1 == "sonar" ]]; then
   echo "run junit, checkstyle and code coverage, and push project data to sonarqube."
   mvn clean checkstyle:checkstyle \
-  org.jacoco:jacoco-maven-plugin:prepare-agent install -Dmaven.test.failure.ignore=true \
+  org.jacoco:jacoco-maven-plugin:prepare-agent package -Dmaven.test.failure.ignore=true \
   sonar:sonar -Dsonar.host.url=http://localhost:9000
 fi
 
